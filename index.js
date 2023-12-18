c8e22ec4b669253c3dac1276aea987449959e662
Itha Sriram Santhosh <isrs444@gmail.com>
2023-12-18 23:54:11 +0530

    basic-setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = process.env.port || 8000;
+
+const db = require('./config/mongoose');
+
+// db();
+
+
+
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static("assets"));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+// app.use("/", require("./routes/index"));
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error occured while running the server at ${port}`);
+    }
+    console.log(`Yup! Successfully running server on ${port}`);
+});
